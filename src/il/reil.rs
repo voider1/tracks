d56f2b78abbf6f6a03c653bd581261b7748cbbde
voider1 <wesleyey0408@gmail.com>
2017-03-03 20:29:01 +0100

    Formatting code the Rustcean

--- src/il/reil.rs
+++ src/il/reil.rs
@@ -1,36 +1,36 @@
 use std::fmt;
 
 pub enum Operand {
-    IntegerLiteral {size: u8, value: i64},
-    Register {index: u64, size: u8},
-    Subaddress {size: u8, value: u64},
+    IntegerLiteral { size: u8, value: i64 },
+    Register { index: u64, size: u8 },
+    Subaddress { size: u8, value: u64 },
 }
 
 pub enum Instruction {
     // Arithmetic instructions
-    Add (Operand, Operand, Operand),
-    Sub (Operand, Operand, Operand),
-    Mul (Operand, Operand, Operand),
-    Div (Operand, Operand, Operand),
-    Mod (Operand, Operand, Operand),
-    Bsh (Operand, Operand, Operand),
+    Add(Operand, Operand, Operand),
+    Sub(Operand, Operand, Operand),
+    Mul(Operand, Operand, Operand),
+    Div(Operand, Operand, Operand),
+    Mod(Operand, Operand, Operand),
+    Bsh(Operand, Operand, Operand),
 
     // Bitwise instructions
-    And (Operand, Operand, Operand),
-    Or (Operand, Operand, Operand),
-    Xor (Operand, Operand, Operand),
+    And(Operand, Operand, Operand),
+    Or(Operand, Operand, Operand),
+    Xor(Operand, Operand, Operand),
 
     // Data transfer instructions
-    Ldm (Operand, Operand),
-    Stm (Operand, Operand),
-    Str (Operand, Operand),
+    Ldm(Operand, Operand),
+    Stm(Operand, Operand),
+    Str(Operand, Operand),
 
     // Conditional instructions
-    Bisz (Operand, Operand),
-    Jcc (Operand, Operand),
+    Bisz(Operand, Operand),
+    Jcc(Operand, Operand),
 
     // Other instructions,
-    Undef (Operand),
+    Undef(Operand),
     Unkn,
     Nop,
 }
@@ -38,17 +38,19 @@ pub enum Instruction {
 impl fmt::Display for Operand {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            Operand::IntegerLiteral{size:s, value:v} => write!(f, "{:x}/b{}", v, s),
-            Operand::Register{index:i, size:s} => write!(f, "t{}/b{}", i, s),
-            Operand::Subaddress{size:s, value:v} => write!(f, "{:x}/b{}", v, s),
+            Operand::IntegerLiteral { size: s, value: v } => write!(f, "{:x}/b{}", v, s),
+            Operand::Register { index: i, size: s } => write!(f, "t{}/b{}", i, s),
+            Operand::Subaddress { size: s, value: v } => write!(f, "{:x}/b{}", v, s),
         }
     }
 }
 
-impl fmt::Display for Instruction{
+impl fmt::Display for Instruction {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            Instruction::Add(ref in1, ref in2, ref out) => write!(f, "add {}, {}, {}", in1, in2, out),
+            Instruction::Add(ref in1, ref in2, ref out) => {
+                write!(f, "add {}, {}, {}", in1, in2, out)
+            }
             _ => write!(f, "unkn"),
         }
     }
--- tests/printers.rs
+++ tests/printers.rs
@@ -5,8 +5,12 @@ use tracks::il::reil::Instruction;
 
 #[test]
 fn print_reil_add() {
-    let instr = Instruction::Add(Operand::IntegerLiteral{size: 1, value: 1}
-    , Operand::IntegerLiteral{size: 1, value: 1}, Operand::Register{index: 1, size: 1});
+    let instr = Instruction::Add(
+        Operand::IntegerLiteral { size: 1, value: 1, },
+        Operand::IntegerLiteral { size: 1, value: 1, },
+        Operand::Register { index: 1, size: 1, }
+    );
+
     let res = format!("{}", instr);
     assert_eq!("add 1/b1, 1/b1, t1/b1", res);
 }
