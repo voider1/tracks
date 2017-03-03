0aa84e34fa81e17835cd27da69ff6ec07315bf21
Remco Vermeulen <remco@hut15.nl>
2017-03-03 22:14:03 +0100

    Add meta-data to instructions
    Meta-data is a simple map of key-value pairs that provides information
    that can be useful during static-code analysis.

--- src/il/reil.rs
+++ src/il/reil.rs
@@ -1,4 +1,7 @@
 use std::fmt;
+use std::collections::HashMap;
+
+pub type MetaData<'a> = HashMap<&'a str, &'a str>;
 
 pub enum Operand {
     IntegerLiteral {size: u8, value: i64},
@@ -7,33 +10,33 @@ pub enum Operand {
     Empty
 }
 
-pub enum Instruction {
+pub enum Instruction<'a> {
     // Arithmetic instructions
-    Add (Operand, Operand, Operand),
-    Sub (Operand, Operand, Operand),
-    Mul (Operand, Operand, Operand),
-    Div (Operand, Operand, Operand),
-    Mod (Operand, Operand, Operand),
-    Bsh (Operand, Operand, Operand),
+    Add (Operand, Operand, Operand, &'a MetaData<'a>),
+    Sub (Operand, Operand, Operand, &'a MetaData<'a>),
+    Mul (Operand, Operand, Operand, &'a MetaData<'a>),
+    Div (Operand, Operand, Operand, &'a MetaData<'a>),
+    Mod (Operand, Operand, Operand, &'a MetaData<'a>),
+    Bsh (Operand, Operand, Operand, &'a MetaData<'a>),
 
     // Bitwise instructions
-    And (Operand, Operand, Operand),
-    Or (Operand, Operand, Operand),
-    Xor (Operand, Operand, Operand),
+    And (Operand, Operand, Operand, &'a MetaData<'a>),
+    Or (Operand, Operand, Operand, &'a MetaData<'a>),
+    Xor (Operand, Operand, Operand, &'a MetaData<'a>),
 
     // Data transfer instructions
-    Ldm (Operand, Operand, Operand),
-    Stm (Operand, Operand, Operand),
-    Str (Operand, Operand, Operand),
+    Ldm (Operand, Operand, Operand, &'a MetaData<'a>),
+    Stm (Operand, Operand, Operand, &'a MetaData<'a>),
+    Str (Operand, Operand, Operand, &'a MetaData<'a>),
 
     // Conditional instructions
-    Bisz (Operand, Operand, Operand),
-    Jcc (Operand, Operand, Operand),
+    Bisz (Operand, Operand, Operand, &'a MetaData<'a>),
+    Jcc (Operand, Operand, Operand, &'a MetaData<'a>),
 
     // Other instructions,
-    Undef (Operand, Operand, Operand),
-    Unkn (Operand, Operand, Operand),
-    Nop (Operand, Operand, Operand),
+    Undef (Operand, Operand, Operand, &'a MetaData<'a>),
+    Unkn (Operand, Operand, Operand, &'a MetaData<'a>),
+    Nop (Operand, Operand, Operand, &'a MetaData<'a>),
 }
 
 impl fmt::Display for Operand {
@@ -47,10 +50,12 @@ impl fmt::Display for Operand {
     }
 }
 
-impl fmt::Display for Instruction{
+impl<'a> fmt::Display for Instruction<'a> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            Instruction::Add(ref in1, ref in2, ref out) => write!(f, "add {}, {}, {}", in1, in2, out),
+            Instruction::Add(ref in1, ref in2, ref out, _) => write!(f, "add {}, {}, {}", in1, in2, out),
+            Instruction::Nop(_, _,_, _) => write!(f, "nop"),
+            Instruction::Unkn(_, _, _, _) => write!(f, "unkn"),
             _ => write!(f, "unkn"),
         }
     }
