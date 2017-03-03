38660b25aba4755ec985a8c432f7c641fc9a58d3
rv <rv@0xcc.local>
2017-03-03 08:21:35 +0100

    Replace instr & op printers with display impl.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 pub enum ReilOperand {
     IntegerLiteral {size: u8, value: i64},
     Register {index: u64, size: u8},
@@ -33,18 +35,21 @@ pub enum ReilInstruction {
     Nop,
 }
 
-fn print_operand(op: ReilOperand) -> String {
-    match op {
-        ReilOperand::IntegerLiteral{size:s, value:v} => format!("{:x}/b{}", v, s),
-        ReilOperand::Register{index:i, size:s} => format!("t{}/b{}", i, s),
-        ReilOperand::Subaddress{size:s, value:v} => format!("{:x}/b{}", v, s),
+impl fmt::Display for ReilOperand {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            ReilOperand::IntegerLiteral{size:s, value:v} => write!(f, "{:x}/b{}", v, s),
+            ReilOperand::Register{index:i, size:s} => write!(f, "t{}/b{}", i, s),
+            ReilOperand::Subaddress{size:s, value:v} => write!(f, "{:x}/b{}", v, s),
+        }
     }
 }
 
-pub fn print_instruction(instr: ReilInstruction) -> String {
-    match instr {
-    ReilInstruction::Add(in1, in2, out) => format!("add {}, {}, {}", print_operand(in1),
-        print_operand(in2), print_operand(out)),
-    _ => "unkn".to_string(),
+impl fmt::Display for ReilInstruction{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            ReilInstruction::Add(ref in1, ref in2, ref out) => write!(f, "add {}, {}, {}", in1, in2, out),
+            _ => write!(f, "unkn"),
+        }
     }
 }
--- tests/printers.rs
+++ tests/printers.rs
@@ -4,6 +4,6 @@ extern crate tracks;
 fn print_add() {
     let instr = tracks::ReilInstruction::Add(tracks::ReilOperand::IntegerLiteral{size: 1, value: 1}
     , tracks::ReilOperand::IntegerLiteral{size: 1, value: 1}, tracks::ReilOperand::Register{index: 1, size: 1});
-    let res = tracks::print_instruction(instr);
+    let res = format!("{}", instr);
     assert_eq!("add 1/b1, 1/b1, t1/b1", res);
 }
