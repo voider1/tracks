420a5ff283e77aab1be146657f33acf29be7707b
voider1 <wesleyey0408@gmail.com>
2017-03-05 21:56:35 +0100

    Reformatting code the Rustacean way

--- src/il/reil.rs
+++ src/il/reil.rs
@@ -4,47 +4,47 @@ use std::collections::HashMap;
 pub type MetaData<'a> = HashMap<&'a str, &'a str>;
 
 pub enum Operand {
-    IntegerLiteral {size: u8, value: i64},
-    Register {index: u64, size: u8},
-    Subaddress {size: u8, value: u64},
-    Empty
+    IntegerLiteral { size: u8, value: i64 },
+    Register { index: u64, size: u8 },
+    Subaddress { size: u8, value: u64 },
+    Empty,
 }
 
 pub enum Instruction<'a> {
     // Arithmetic instructions
-    Add (Operand, Operand, Operand, &'a MetaData<'a>),
-    Sub (Operand, Operand, Operand, &'a MetaData<'a>),
-    Mul (Operand, Operand, Operand, &'a MetaData<'a>),
-    Div (Operand, Operand, Operand, &'a MetaData<'a>),
-    Mod (Operand, Operand, Operand, &'a MetaData<'a>),
-    Bsh (Operand, Operand, Operand, &'a MetaData<'a>),
+    Add(Operand, Operand, Operand, &'a MetaData<'a>),
+    Sub(Operand, Operand, Operand, &'a MetaData<'a>),
+    Mul(Operand, Operand, Operand, &'a MetaData<'a>),
+    Div(Operand, Operand, Operand, &'a MetaData<'a>),
+    Mod(Operand, Operand, Operand, &'a MetaData<'a>),
+    Bsh(Operand, Operand, Operand, &'a MetaData<'a>),
 
     // Bitwise instructions
-    And (Operand, Operand, Operand, &'a MetaData<'a>),
-    Or (Operand, Operand, Operand, &'a MetaData<'a>),
-    Xor (Operand, Operand, Operand, &'a MetaData<'a>),
+    And(Operand, Operand, Operand, &'a MetaData<'a>),
+    Or(Operand, Operand, Operand, &'a MetaData<'a>),
+    Xor(Operand, Operand, Operand, &'a MetaData<'a>),
 
     // Data transfer instructions
-    Ldm (Operand, Operand, Operand, &'a MetaData<'a>),
-    Stm (Operand, Operand, Operand, &'a MetaData<'a>),
-    Str (Operand, Operand, Operand, &'a MetaData<'a>),
+    Ldm(Operand, Operand, Operand, &'a MetaData<'a>),
+    Stm(Operand, Operand, Operand, &'a MetaData<'a>),
+    Str(Operand, Operand, Operand, &'a MetaData<'a>),
 
     // Conditional instructions
-    Bisz (Operand, Operand, Operand, &'a MetaData<'a>),
-    Jcc (Operand, Operand, Operand, &'a MetaData<'a>),
+    Bisz(Operand, Operand, Operand, &'a MetaData<'a>),
+    Jcc(Operand, Operand, Operand, &'a MetaData<'a>),
 
     // Other instructions,
-    Undef (Operand, Operand, Operand, &'a MetaData<'a>),
-    Unkn (Operand, Operand, Operand, &'a MetaData<'a>),
-    Nop (Operand, Operand, Operand, &'a MetaData<'a>),
+    Undef(Operand, Operand, Operand, &'a MetaData<'a>),
+    Unkn(Operand, Operand, Operand, &'a MetaData<'a>),
+    Nop(Operand, Operand, Operand, &'a MetaData<'a>),
 }
 
 impl fmt::Display for Operand {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            Operand::IntegerLiteral{size:s, value:v} => write!(f, "{:x}/b{}", v, s),
-            Operand::Register{index:i, size:s} => write!(f, "t{}/b{}", i, s),
-            Operand::Subaddress{size:s, value:v} => write!(f, "{:x}/b{}", v, s),
+            Operand::IntegerLiteral { size: s, value: v } => write!(f, "{:x}/b{}", v, s),
+            Operand::Register { index: i, size: s } => write!(f, "t{}/b{}", i, s),
+            Operand::Subaddress { size: s, value: v } => write!(f, "{:x}/b{}", v, s),
             Operand::Empty => write!(f, ""),
         }
     }
@@ -53,8 +53,10 @@ impl fmt::Display for Operand {
 impl<'a> fmt::Display for Instruction<'a> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            Instruction::Add(ref in1, ref in2, ref out, _) => write!(f, "add {}, {}, {}", in1, in2, out),
-            Instruction::Nop(_, _,_, _) => write!(f, "nop"),
+            Instruction::Add(ref in1, ref in2, ref out, _) => {
+                write!(f, "add {}, {}, {}", in1, in2, out)
+            }
+            Instruction::Nop(_, _, _, _) => write!(f, "nop"),
             Instruction::Unkn(_, _, _, _) => write!(f, "unkn"),
             _ => write!(f, "unkn"),
         }
--- tests/printers.rs
+++ tests/printers.rs
@@ -7,9 +7,13 @@ use tracks::il::reil::MetaData;
 #[test]
 fn print_reil_add() {
     let meta = MetaData::new();
-    let instr = Instruction::Add(Operand::IntegerLiteral{size: 1, value: 1}
-    , Operand::IntegerLiteral{size: 1, value: 1}, Operand::Register{index: 1, size: 1}
-    , &meta);
+    let instr = Instruction::Add(
+        Operand::IntegerLiteral { size: 1, value: 1, },
+        Operand::IntegerLiteral { size: 1, value: 1, },
+        Operand::Register { index: 1, size: 1, },
+        &meta
+    );
+
     let res = format!("{}", instr);
     assert_eq!("add 1/b1, 1/b1, t1/b1", res);
 }
